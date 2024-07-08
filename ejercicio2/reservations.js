673ff11d78e64628e4ced1b6357f64c13d23452b
Marian <agostina_novotny@hotmail.com>
2024-07-07 22:16:09 -0300

    Ejercicio 2 Terminado (Implementación de las clases Customer y Reservation)

--- ejercicio2/reservations.js
+++ ejercicio2/reservations.js
@@ -1,6 +1,44 @@
-class Customer {}
+class Customer {
+    constructor(id, name, email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 
-class Reservation {}
+    get info() {
+        return `${this.name} (${this.email})`;
+    }
+}
+class Reservation {
+    constructor(id, customer, date, guests) {
+        this.id = id;
+        this.customer = customer;
+        this.date = new Date(date);
+        this.guests = guests;
+    }
+
+    get info() {
+        return `
+        Cliente: ${this.customer.info}<br>
+        Fecha y hora: ${this.date.toLocaleString()}<br>   
+        Comensales: ${this.guests}
+          `;
+    
+    }
+
+    static validateReservation(date, guests) {
+        const reservationDate = new Date(date);
+        if (reservationDate < new Date() || guests <= 0) {
+            return false;
+        }
+        return true;
+    }
+
+
+
+
+
+}
 
 class Restaurant {
     constructor(name) {
